44c193d200ffdb733818c02aefcb0ede3d0e8a63
nkhanhcong <nkhanhcong@gmail.com>
2024-03-19 13:54:11 +0700

    can visit all rooms

--- DFS/KeysAndRooms.go
+++ DFS/KeysAndRooms.go
@@ -1,1 +1,21 @@
 package dfs
+
+func CanVisitAllRooms(rooms [][]int) bool {
+	stackLockedRooms := []int{0}
+	visitedRooms := map[int]bool{0: true}
+
+	for len(stackLockedRooms) > 0 {
+
+		room := stackLockedRooms[len(stackLockedRooms)-1]
+		stackLockedRooms = stackLockedRooms[:len(stackLockedRooms)-1]
+
+		for _, nextRoom := range rooms[room] {
+			if !visitedRooms[nextRoom] {
+				visitedRooms[nextRoom] = true
+				stackLockedRooms = append(stackLockedRooms, nextRoom)
+			}
+		}
+	}
+
+	return len(rooms) == len(visitedRooms)
+}
--- DFS/KeysAndRooms_test.go
+++ DFS/KeysAndRooms_test.go
@@ -1,2 +1,16 @@
 package dfs
 
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCanTravelAllRoom(t *testing.T) {
+	rooms := [][]int{[]int{1}, []int{2}, []int{3}, []int{}}
+
+	res := CanVisitAllRooms(rooms)
+
+	assert.Equal(t, true, res)
+
+}
