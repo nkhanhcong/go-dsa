dc7ad53086bbd3da95a7ba9935dfffe2607009ea
nkhanhcong <nkhanhcong@gmail.com>
2024-03-29 07:47:31 +0700

    minimum fuel cost

--- DFS/MinimumFuelCost.go
+++ DFS/MinimumFuelCost.go
@@ -1,39 +1,53 @@
 package dfs
 
+import (
+	"fmt"
+)
 
-func MinimumFuelCost(roads [][]int, seats int) int64{
+func MinimumFuelCost(roads [][]int, seats int) int64 {
 	graph := makeGraphCity(roads)
 	visitedMap := make(map[int]bool)
+	ans := int64(0)
 
-	count:= dfsCity(graph,0, visitedMap,0)
+	count, result := dfsCity(graph, 0, visitedMap, seats, ans)
 
-	return int64(count)
+	fmt.Println(count, ans)
+	return int64(result)
 
 }
 
-func dfsCity(graph map[int][]int, node int, visited map[int]bool, count int)int{
+func dfsCity(graph map[int][]int, node int, visited map[int]bool, seats int, ans int64) (int, int64) {
 
-	_,exsited := visited[node]
-	if exsited{
-		return 0
+	_, exsited := visited[node]
+
+	if exsited {
+		return 0, int64(0)
 	}
 
 	visited[node] = true
 
-	for _, neigh := range graph[node]{
-		if !visited[neigh]{
-			dfsCity(graph,neigh,visited,count+1)
+	totalPeople := 0
+	for _, neigh := range graph[node] {
+		if visited[neigh] {
+			continue
 		}
+
+		people, _ := dfsCity(graph, neigh, visited, seats,ans)
+		ans += int64(people / seats)
+		totalPeople += people
+
 	}
 
-	return 0
+	fmt.Println(totalPeople, node)
+
+	return totalPeople + 1, ans
 
 }
 
-func makeGraphCity(roads [][]int) map[int][]int{
+func makeGraphCity(roads [][]int) map[int][]int {
 	graph := make(map[int][]int)
 
-	for _, r := range roads{
+	for _, r := range roads {
 		from, to := r[0], r[1]
 
 		graph[from] = append(graph[from], to)
--- DFS/MinimumFuelCost_test.go
+++ DFS/MinimumFuelCost_test.go
@@ -5,18 +5,17 @@ import (
 	"testing"
 )
 
-
-func TestMinimumFuelCost(t *testing.T){
+func TestMinimumFuelCost(t *testing.T) {
 	roads := [][]int{
-		{3,1},
-		{3,2},
-		{1,0},
-		{0,4},
-		{0,5},
-		{4,6},
+		{3, 1},
+		{3, 2},
+		{1, 0},
+		{0, 4},
+		{0, 5},
+		{4, 6},
 	}
 
-	seats:=2
+	seats := 2
 
-	fmt.Println(MinimumFuelCost(roads,seats))
+	fmt.Println(MinimumFuelCost(roads, seats))
 }
