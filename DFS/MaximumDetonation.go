9fa4ab791b8df5d8d1ab586542b8f0068d9c7950
nkhanhcong <nkhanhcong@gmail.com>
2024-04-01 00:27:32 +0700

    dfs bomb map

--- DFS/MaximumDetonation.go
+++ DFS/MaximumDetonation.go
@@ -6,11 +6,34 @@ import (
 )
 
 func MaximumDenotation(bombs [][]int) int {
-
 	graph := buildGraphBomb(bombs)
-	fmt.Println(graph)
 
-	return 0
+	maximum := 1
+
+	for i := range graph {
+		visitedMap := make(map[int]bool)
+		dfsBombMap(graph, i, visitedMap)
+		maximum = max(maximum, len(visitedMap))
+	}
+
+	fmt.Println("Maximum Detonation:", maximum) // Print the maximum detonation value
+
+	return maximum
+}
+
+func dfsBombMap(graph map[int][]int, node int, visitedMap map[int]bool) {
+	if visitedMap[node] {
+		return
+	}
+
+	visitedMap[node] = true
+
+	for _, neigh := range graph[node] {
+		if !visitedMap[neigh] {
+			dfsBombMap(graph, neigh, visitedMap)
+		}
+	}
+
 }
 
 func buildGraphBomb(bombs [][]int) map[int][]int {
@@ -38,5 +61,5 @@ func isConnectedBomb(bombA []int, bombB []int) bool {
 
 	distanceAB := math.Sqrt(math.Pow(float64(xA)-float64(xB), 2) + math.Pow(float64(yA)-float64(yB), 2))
 
-	return distanceAB < float64(rA+rB)
+	return distanceAB <= float64(rA) || distanceAB <= float64(rB)
 }
---
+++ DFS/MaximumDetonation_test.go
@@ -0,0 +1,37 @@
+package dfs
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMaximumDenotation(t *testing.T) {
+	bombs := [][]int{
+		{2, 1, 3},
+		{6, 1, 4},
+	}
+
+	assert.Equal(t, 2, MaximumDenotation(bombs))
+}
+
+func TestMaximumDenotationWithoutConnected(t *testing.T) {
+	bombs := [][]int{
+		{1, 1, 5},
+		{10, 10, 5},
+	}
+
+	assert.Equal(t, 1, MaximumDenotation(bombs))
+}
+
+func TestMaximumDenotationWith5Connected(t *testing.T) {
+	bombs := [][]int{
+		{1, 2, 3},
+		{2, 3, 1},
+		{3, 4, 2},
+		{4, 5, 3},
+		{5, 6, 4},
+	}
+
+	assert.Equal(t, 5, MaximumDenotation(bombs))
+}
