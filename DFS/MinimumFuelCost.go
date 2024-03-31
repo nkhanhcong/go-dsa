afe03b30fec6d971293d21f7fae16a41e6a3d1c6
nkhanhcong <nkhanhcong@gmail.com>
2024-03-31 09:51:16 +0700

    minimum fuel cost 2

--- DFS/MinimumFuelCost.go
+++ DFS/MinimumFuelCost.go
@@ -1,41 +1,73 @@
 package dfs
 
-import "fmt"
+import (
+	"math"
+)
 
 func MinimumFuelCost(roads [][]int, seats int) int {
 
 	graph := makeGraphCity(roads)
+	var dfsCity func(node int, parrent int) (int, int)
+	dfsCity = func(node int, parent int) (int, int) {
 
-	res := 0
-
-	var dfsCity func(node int, parrent int) int
-	dfsCity = func(node int, parent int) int {
-
-		totalPeople := 0
+		totalPeople := 1
+		localCost := 0
 
 		for _, child := range graph[node] {
 
 			if child != parent {
 
-				people := dfsCity(child, node)
+				people, cost := dfsCity(child, node)
 				totalPeople += people
 
-				res += (totalPeople / seats)
-				fmt.Println(totalPeople/seats, totalPeople, seats)
-				if totalPeople%seats != 0 && parent != 1 {
-					res += 1
-				}
+				localCost += cost
+			}
+		}
+		trips := totalPeople / seats
+
+		if totalPeople%seats != 0 {
+			trips++
+		}
+
+		if node != 0 {
+			localCost += trips
+
+		}
+
+		return totalPeople, localCost
+
+	}
+
+	_, res := dfsCity(0, -1)
+
+	return res
+
+}
+
+func MinimumFuelCost2(roads [][]int, seats int) int64 {
+	graph := makeGraphCity(roads)
+	res := int64(0)
+
+	var dfsCity func(node int, parent int) int
+	dfsCity = func(node int, parent int) int {
+		totalPeople := 1
+		for _, child := range graph[node] {
+			if child != parent {
+				people := dfsCity(child, node)
+				totalPeople += people
 			}
 		}
 
-		return totalPeople + 1
+		if node != 0 {
+			res += int64(math.Ceil(float64(totalPeople) / float64(seats)))
+		}
+		return totalPeople
 
 	}
 
 	dfsCity(0, -1)
 
 	return res
-
 }
 
 func makeGraphCity(roads [][]int) map[int][]int {
--- DFS/MinimumFuelCost_test.go
+++ DFS/MinimumFuelCost_test.go
@@ -1,7 +1,6 @@
 package dfs
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -22,6 +21,9 @@ func TestMinimumFuelCost(t *testing.T) {
 	ans := MinimumFuelCost(roads, seats)
 
 	assert.Equal(t, 7, ans)
+
+	ans2 := MinimumFuelCost2(roads, seats)
+	assert.Equal(t, int64(7), ans2)
 }
 
 func TestMinimumFuelCostWithNone(t *testing.T) {
@@ -29,5 +31,19 @@ func TestMinimumFuelCostWithNone(t *testing.T) {
 
 	seats := 2
 
-	fmt.Println(MinimumFuelCost(roads, seats))
+	assert.Equal(t, 0, MinimumFuelCost(roads, seats))
+	assert.Equal(t, int64(0), MinimumFuelCost2(roads, seats))
+}
+
+func TestTravelOnlyOneOnEachNode(t *testing.T) {
+	roads := [][]int{
+		{0, 1},
+		{0, 2},
+		{0, 3},
+	}
+
+	seats := 5
+
+	assert.Equal(t, 3, MinimumFuelCost(roads, seats))
+	assert.Equal(t, int64(3), MinimumFuelCost2(roads, seats))
 }
