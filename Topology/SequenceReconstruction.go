ee83dad57b93bbfd54609cb2a33f1b55ddf7a63f
nkhanhcong <nkhanhcong@gmail.com>
2024-05-05 22:37:49 +0700

    testing

--- Topology/SequenceReconstruction.go
+++ Topology/SequenceReconstruction.go
@@ -2,52 +2,51 @@ package topology
 
 import "fmt"
 
-func SequenceReconstruction(nums []int, sequences [][]int)bool{
-	lengthNums := len(nums)+1
+func SequenceReconstruction(nums []int, sequences [][]int) bool {
+	lengthNums := len(nums) + 1
 	indegree := make([]int, lengthNums)
-	graph := map[int][]int{} 
-	
-	for _, s := range sequences{
-		for i:= 0; i < len(s) -1;{
-			fmt.Println("i: ",i)
+	graph := map[int][]int{}
+
+	for _, s := range sequences {
+		for i := 0; i < len(s)-1; {
+			fmt.Println("i: ", i)
 			u := s[i]
 			v := s[i+1]
-			indegree[v] +=1
+			indegree[v] += 1
 			graph[u] = append(graph[u], v)
-			i = i+ 1
-			
+			i = i + 1
+
 		}
 	}
 
 	queueSlice := []int{}
 
-	for i,v:= range indegree{
-		if v == 0 && i !=0{
+	for i, v := range indegree {
+		if v == 0 && i != 0 {
 			queueSlice = append(queueSlice, v)
 		}
 	}
 
-	if len(queueSlice) !=1 {
+	if len(queueSlice) != 1 {
 		return false
 	}
 
 	fmt.Println("graph: ", graph)
-	for len(queueSlice) >0{
+	fmt.Println("indegree: ", indegree)
+	for len(queueSlice) > 0 {
 		q := queueSlice[0]
 		queueSlice = queueSlice[1:]
-		for _, s := range graph[q]{
-			indegree[s] -=1
-			if indegree[s] ==0{
+		for _, s := range graph[q] {
+			indegree[s] -= 1
+			if indegree[s] == 0 {
 				queueSlice = append(queueSlice, s)
 			}
-
-			if len(queueSlice) >1{
-				return false
-			}
+		}
+		if len(queueSlice) > 1 {
+			return false
 		}
 	}
 
-
-	return true 
+	return true
 
 }
