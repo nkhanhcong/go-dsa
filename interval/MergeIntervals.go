124f16e3170f2dae7158237a3289f0c34ee4449f
nkhanhcong <nkhanhcong@gmail.com>
2024-05-20 23:58:06 +0700

    handle merge with 2 loop

--- interval/MergeIntervals.go
+++ interval/MergeIntervals.go
@@ -1,5 +1,7 @@
 package interval
 
+import "fmt"
+
 
 func MergeInterval(intervals [][]int) [][]int{
 	res := [][]int{}
@@ -9,8 +11,29 @@ func MergeInterval(intervals [][]int) [][]int{
 		if len(res) == 0 {
 			res = append(res, interval)
 		}else{
-			lastInterval = res[len(res)-1]
+			lastInterval := res[len(res)-1]
 			res = res[0:len(res)-1]
+			if IsOverlapIntervals(interval, lastInterval){
+				left := min(interval[0], lastInterval[0])
+				right := max(interval[1],lastInterval[1])
+				res = append(res, []int{left,right})
+				for len(res)> 1 {
+					lastInterval := res[len(res) -1]
+					nearLastInterval := res[len(res)-2]
+					res = res[0:len(res)-2]
+					if IsOverlapIntervals(lastInterval,nearLastInterval){
+						left := min(lastInterval[0], nearLastInterval[0])
+						right := max(lastInterval[1], nearLastInterval[1])
+						res = append(res, []int{left,right})
+					}else{
+						break
+					}
+				}
+			}else{
+				res = append(res, lastInterval)
+				res = append(res, interval)
+			}
+			
 		}
 
 	}
@@ -27,5 +50,5 @@ func IsLeftIntervals(a []int, b []int) bool{
 }
 
 func IsOverlapIntervals(a []int, b[]int)bool{
-	return !IsLeftIntervals(a,b) || !IsRightIntervals(a,b)
+	return !IsLeftIntervals(a,b) && !IsRightIntervals(a,b)
 }
--- interval/MergeIntervals_test.go
+++ interval/MergeIntervals_test.go
@@ -1,15 +1,34 @@
 package interval
 
+import (
+	"fmt"
+	"testing"
+)
 
-func TestMergeIntervals(){
+func TestMergeIntervals(t *testing.T) {
 	intervals := [][]int{
-		{1,3},
-		{2,6},
-		{8,10},
-		{15,18},
+		{1, 3},
+		{2, 6},
+		{8, 10},
+		{15, 18},
 	}
 
-	res :=MergeInterval(intervals)
+	res := MergeInterval(intervals)
 
-	println(res)
+	fmt.Println(res)
+}
+
+func TestMergeWithOneArrayResult(t *testing.T) {
+	intervals := [][]int{
+		{2,3},
+		{4,5},
+		{6,7},
+		{8,9},
+		{1,10},
+	}
+
+	res := MergeInterval(intervals)
+
+	fmt.Println(res)
+	
 }
